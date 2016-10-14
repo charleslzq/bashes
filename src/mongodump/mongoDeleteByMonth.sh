a9100d032c6d5d22b56cff29ad6b85a0b387089f
charles <charleslzq@gmail.com>
2016-10-14 09:27:12 +0800

    完善输出信息

--- src/mongodump/mongoDeleteByMonth.sh
+++ src/mongodump/mongoDeleteByMonth.sh
@@ -2,6 +2,7 @@
 . ./config.sh
 collection=$1
 month=$2
-echo "try to delete "${collection}" in month "${month}
+time=`date "+%Y-%m-%d %H:%M:%S"`
+echo ${time}" try to delete "${collection}" in month "${month}
 cd ${mongo_path}
 ./mongo.exe ${db} --eval "db.getCollection(\"${collection}\").remove({month:${month}})"
